11330593d6c3b14e85095a0821013af9009c5ab1
tksundar <53440744+tksundar@users.noreply.github.com>
2020-09-25 18:28:50 +0530

    new files

--- app/components/EventRegistration.js
+++ app/components/EventRegistration.js
@@ -7,6 +7,37 @@ import { getRemoteData } from './Util';
 const EventRegistration = ({route,navigation}) =>{
 
     const eventDetail = route.params
+
+    const styles = StyleSheet.create({
+        hflex:{
+            flex:1,
+            flexDirection:'row',
+            margin: 5,
+            alignItems: "flex-start",
+            justifyContent: "space-evenly"
+        },
+
+        vFlex:{
+            flexDirection:'column',
+           
+            justifyContent:'space-evenly'
+        },
+
+        banner:{
+          flex:0.2,
+          justifyContent:'center',
+          backgroundColor: 'grey' ,
+          alignItems:'flex-end',
+          margin:10,
+        },
+        item:{
+            height: 40,
+            width: 200, 
+            borderColor: 'gray',
+            borderWidth: 1
+        },
+    })
+
    
     return(
         <>
