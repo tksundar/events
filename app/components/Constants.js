c6d646dde90078d5aef9cf770b47624be2e21378
Sundar Krishnamachari <53440744+tksundar@users.noreply.github.com>
2020-10-28 16:41:30 +0530

    Update Constants.js

--- app/components/Constants.js
+++ app/components/Constants.js
@@ -1,12 +1,3 @@
-// export const URL_LOGIN="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000/events/login";
-// export const URL_EVENTS="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000/events";
-// export const URL_NEW_USER="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000/events/register";
-// export const URL_EVENT_REGISTRATION="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000/events/create";
-// export const URL_CHECK_USER="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000/events/check";
-// export const URL_DISPLAY_MEDIA="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000/events/display";
-// export const URL_PREFIX="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000";
-// export const URL_UPLOAD_MEDIA="http://ec2-13-232-233-180.ap-south-1.compute.amazonaws.com:8000/events/upload";
-
 //export const URL_PREFIX = "http://ec2-65-0-61-175.ap-south-1.compute.amazonaws.com:8000";
 export const URL_PREFIX = "http://192.168.0.100:8000";
 export const URL_LOGIN = URL_PREFIX + "/events/login";
